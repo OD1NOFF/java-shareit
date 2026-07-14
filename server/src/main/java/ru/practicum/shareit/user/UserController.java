c8405543c01e2c2bf69fd09a1a5653de2e13f885
OD1N <volodin12340987@gmail.com>
2026-07-14 16:31:59 +0300

    Minor CHECKSTYLE ٩(ˋᗣˊ*)و fixes

--- server/src/main/java/ru/practicum/shareit/user/UserController.java
+++ server/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.user;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
